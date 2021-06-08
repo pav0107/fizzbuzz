082bc99ce8100d7a83789d92f06a70ee99429b00
Pav Rao <pav0107@gmail.com>
2021-06-08 15:16:26 +0100

    added tests for 5 and 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
