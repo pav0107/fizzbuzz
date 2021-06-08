81895f18db4b31e7918a1931d168306803101cf0
Pav Rao <pav0107@gmail.com>
2021-06-08 15:39:41 +0100

    added last few tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 15 == 0
     'fizzbuzz'
-  elsif number == 5
+  elsif number % 5 == 0
     'buzz'
   elsif number % 3 == 0
     'fizz'
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,11 +6,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(6)).to eq 'fizz'
     expect(fizzbuzz(9)).to eq 'fizz'
   end
-  it 'return "buzz" when passed 5' do
+  it 'return "buzz" when multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
-  it 'return "fizzbuzz" when passed 15' do
+  it 'return "fizzbuzz" when multiples of 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
   end
   it 'return 4 when passed 4' do
     expect(fizzbuzz(4)).to eq 4
